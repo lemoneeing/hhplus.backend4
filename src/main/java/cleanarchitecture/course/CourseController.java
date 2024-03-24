4b3629c7e95eeb2125037da1a701120b7cf076ab
kjh <jh2two@gmail.com>
2024-03-24 13:43:28 +0900

    Feature: Project 생성 ~ CourseController 추가

---
+++ src/main/java/cleanarchitecture/course/CourseController.java
@@ -0,0 +1,9 @@
+package cleanarchitecture.course;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class CourseController {
+}
