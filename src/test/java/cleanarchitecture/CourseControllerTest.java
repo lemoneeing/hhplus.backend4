921986a04141d5c00c8ca1a0b97c46fa98313909
jh <jh2two@gmail.com>
2024-03-24 22:22:10 +0900

    Test: root url test method 추가, 강의 조회 url test method 추가 (내용 구현 X, 틀만 잡아둠.)

---
+++ src/test/java/cleanarchitecture/CourseControllerTest.java
@@ -0,0 +1,22 @@
+package cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseControllerTest {
+
+    CourseController controller = new CourseController();
+
+    @Test
+    void 루트_URL은_특강_조회로_리다이렉트(){
+
+    }
+
+    @Test
+    void 특강_조회() {
+        // "course/list" 로 Get 요청이 들어오면 모든 특강의 목록이 보여야 함.
+    }
+
+
+}
