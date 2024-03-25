c3d0b641d221a82a7872bdaea929ca0df5f63c61
jh <jh2two@gmail.com>
2024-03-25 23:44:09 +0900

    Chore: 특강 Entity 의 Interface 를 사용하지 않고 구현체 먼저 사용하도록 변경
    - 아직 Entity 를 Interface 를 구현하여 사용하는 방법을 잘 모르겠으므로 구현체부터 사용하는 방식으로 변경

--- src/main/java/cleanarchitecture/course/Course.java
+++ src/main/java/cleanarchitecture/course/Course.java
@@ -1,3 +1,34 @@
 package cleanarchitecture.course;
 
-public interface Course {}
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Course {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long courseId;
+
+    @Column(length=50)
+    public String courseName;
+
+    @Column
+    public Integer capacity;
+
+    @Column
+    public List<Long> applicants; // DB Column 에 List type 을 사용해도 되는지...?
+
+    @Column
+    @DateTimeFormat(pattern="yy-MM-dd HH:mm")
+//    @JsonFormat(pattern="yy-MM-dd HH:mm")
+    public LocalDateTime date;
+
+}
--- src/main/java/cleanarchitecture/course/Workshop.java
+++ src/main/java/cleanarchitecture/course/Workshop.java
@@ -7,23 +7,23 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Entity
-@Getter
-public class Workshop implements Course{
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long courseId;
-
-    @Column(length=50)
-    public String courseName;
-
-    @Column
-    public Integer capacity;
-
-    @Column
-    public List<Long> applicants; // DB Column 에 List type 을 사용해도 되는지...?
-
-    @Column
-    @DateTimeFormat(pattern="yyyy-MM-dd")
-    public LocalDateTime date;
-}
+//@Entity
+//@Getter
+//public class Workshop implements Course{
+//    @Id
+//    @GeneratedValue(strategy= GenerationType.IDENTITY)
+//    private Long courseId;
+//
+//    @Column(length=50)
+//    public String courseName;
+//
+//    @Column
+//    public Integer capacity;
+//
+//    @Column
+//    public List<Long> applicants; // DB Column 에 List type 을 사용해도 되는지...?
+//
+//    @Column
+//    @DateTimeFormat(pattern="yyyy-MM-dd")
+//    public LocalDateTime date;
+//}
