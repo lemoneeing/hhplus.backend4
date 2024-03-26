db63bd210bbf49a75ab4764d34ec1534d9c59212
jh <jh2two@gmail.com>
2024-03-26 22:23:34 +0900

    Feat: User entity class 에 column 추가 (userId, reservedCourseId)

--- src/main/java/cleanarchitecture/user/User.java
+++ src/main/java/cleanarchitecture/user/User.java
@@ -1,4 +1,18 @@
 package cleanarchitecture.user;
 
-public interface User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column
+    public Long reservedCourseId;
+
 }
