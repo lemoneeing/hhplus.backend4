b7a55a31cc9bf1f89a37c7ad4df24322a5d212c6
jh <jh2two@gmail.com>
2024-03-29 00:49:34 +0900

    Feat: Course 관련 Business Layer 구현

--- src/main/java/cleanarchitecture/business/CourseService.java
+++ src/main/java/cleanarchitecture/business/CourseService.java
@@ -1,7 +1,9 @@
 package cleanarchitecture.business;
 
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
+// Course 를 모르게 하고 싶음.
 @Service
 public class CourseService {
     public final ICourseRepository repository;
@@ -10,15 +12,40 @@ public class CourseService {
         this.repository = repository;
     }
 
-    // 신청하려는 특강이 신청 가능한 상태인지 확인
-    public Long isCourseReservable(Long courseId) {
-        if(repository.findCountOfApplicantsByCourseId(courseId) < repository.findByCourseId(courseId).getCapacity()) return courseId;
-        else return -1L; // courseID 는 무조건 양수여야 한다는 규칙 필요
+    // 특강 추가 후 특강 Id 반환
+    public Long createCourse(String courseName, Integer courseCapacity, LocalDateTime courseDate){
+        return repository.create(courseName, courseCapacity, courseDate).getCourseId();
+    }
+
+    // 해당 ID 를 갖는 특강의 존재 여부 반환
+    public Boolean doesCourseExist(Long courseId){
+        if (repository.findByCourseId(courseId).isPresent()) return true;
+        else return false;
+    }
+    
+    // 특강명 반환. 조회 관련 로직
+    public String findCourseNameByCourseID(Long courseId){
+        return repository.convertCourse(repository.findByCourseId(courseId)).getCourseName();
+    }
+    
+    
+    // 신청 관련 로직
+    // 신청하려는 특강에 여석이 남아 있는 상태인지 확인
+    public Boolean CourseHasSpace(Long courseId) {
+        // 현재까지의 특강 신청자 수 +1 < 특강 정원
+        if (repository.findCountOfApplicants(courseId) + 1
+                < repository.convertCourse(repository.findByCourseId(courseId)).getCapacity())
+            return true;
+        else
+            return false;
     }
 
     // 특강 신청자 수 증가
-    public Integer addCourseCountOfApplicants(Long courseId){
-        repository.addCountOfApplicants(repository.findByCourseId(courseId));
-        return repository.findCountOfApplicantsByCourseId(courseId);
+    public Integer addCourseApplicants(Long courseId){
+        return repository.addCountOfApplicants(courseId);
     }
 }
+
+//class CourseManager {
+//
+//}
--- src/main/java/cleanarchitecture/business/ICourseRepository.java
+++ src/main/java/cleanarchitecture/business/ICourseRepository.java
@@ -1,12 +1,25 @@
 package cleanarchitecture.business;
 
-import cleanarchitecture.data.Course;
+import cleanarchitecture.persistence.Course;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface ICourseRepository {
-    Course save(Course course);
-    Course findByCourseId(Long courseId);
-    Integer findCountOfApplicantsByCourseId(Long courseId);
-    Integer addCountOfApplicants(Course course);
+    // 강의 추가
+    Course create(String courseName, Integer capacity, LocalDateTime courseDate);
+
+    // 강의 ID 로 Course 객체 탐색 - jpaRepository 의 findById 사용
+    Optional<Course> findByCourseId(Long courseId);
+
+    // jpaRepo 의 findById 반환 Type 인 Optional 을 온갖군데에서 처리해줘야 하는 귀찮음 해결용
+    Course convertCourse(Optional<Course> opt);
+
+    // 현재까지의 신청자 수 반환
+    Integer findCountOfApplicants(Long courseId);
+
+    // 신청자 수 1 증가
+    Integer addCountOfApplicants(Long courseId);
 }
